71d466f7ecba0bf555e817f634e2b9378dfd35e0
ricacnsg <ricamaegconsigo@gmail.com>
2025-11-11 00:05:31 +0800

    Fix title typo in admin_management.php

--- admin_management_system/admin_management.php
+++ admin_management_system/admin_management.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['staff_username'])) {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Daven's Kitchenette - Management Systzm</title>
+    <title>Daven's Kitchenette - Management System</title>
     <link rel="stylesheet" href="/bootstrap5/css/bootstrap.min.css">
     <link rel="stylesheet" href="admin_management.css" />
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
